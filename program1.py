13482e54838b5900a2bb5c690a4deea2819dbf4b
Rudraksh kumawat <Rudrakshkumawat919@gmail.com>
2024-12-07 19:54:09 +0530

    ad

--- program1.py
+++ program1.py
@@ -1,5 +1,8 @@
 import numpy as np
 
 arr3 = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+newarr3 = arr3[:2 ,1:3]
 
-print(np.array([arr3[0][1:3], arr3[1][1:3]]))
+newarr3[0,0] =22
+
+print(arr3)
