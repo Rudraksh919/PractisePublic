bb7f388ed1d6829b5f8f8ad2f8a89af84adc89ba
Rudraksh kumawat <Rudrakshkumawat919@gmail.com>
2024-12-07 19:08:56 +0530

    numpy

--- program2.py
+++ program2.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-arr = np.array([[1,2,3,4,5],[6,7,8,9,10]])
+arr2 = np.array([[1,2,3,4,5],[6,7,8,9,10]])
 
-arr[1][2] = 55
+arr2[1][2] = 55
 
-print(arr.shape)
+print(arr2.shape)
