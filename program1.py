4ee88f28203e5dba72062d62823d4683b4aaf87d
Rudraksh kumawat <Rudrakshkumawat919@gmail.com>
2024-12-08 12:07:32 +0530

    patch to Task_1_Pandas

--- program1.py
+++ program1.py
@@ -1,3 +1,13 @@
 import pandas as pd
 import numpy as np
 import seaborn as sb
+import openpyxl
+
+feb =  pd.read_excel('https://github.com/CodeSolid/CodeSolid.github.io/raw/main/booksource/data/AnalyticsSnapshot.xlsx',sheet_name='February 2022',index_col=0)
+
+
+address = ['NewYork', 'Chicago', 'Boston']
+
+# Using 'Address' as the column name
+# and equating it to the list
+feb['Address'] = address
