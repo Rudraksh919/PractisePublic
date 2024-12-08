180b34bac34a47d61bc331136369f690d8091ee2
Rudraksh kumawat <Rudrakshkumawat919@gmail.com>
2024-12-08 10:59:25 +0530

    add csv

--- program1.py
+++ program1.py
@@ -1,14 +1,3 @@
+import pandas as pd
 import numpy as np
-
-b = np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
-
-boolb = (b%3 == 0)
-c = np.array([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]])
-i = 0
-for i in range(4):
-    j=0
-    for j in range(4):
-        if boolb[i,j] == True:
-            c[i,j] = np.array([b[i,j]])
-
-print(c)
+import seaborn as sb
